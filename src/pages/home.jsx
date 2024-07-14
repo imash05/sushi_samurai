1afe0a8cf47f6c7bd33f32bf07e7ca9aea056e48
IFer <76787367+imash05@users.noreply.github.com>
2024-07-14 21:00:36 +0200

    add code

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 import { Link, NavLink } from "react-router-dom";
 
 const Home = () => {
@@ -151,15 +152,80 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div>food slider</div>
       <div>
-        food slider
+        sezione esperienza ristorante 
       </div>
       <div>
-
+        sezione prenotazioni 
       </div>
-      <div></div>
-      <div></div>
 
+      <footer className="footer">
+        <div className="footer_download">
+          <img src="" alt="" />
+          <div>
+            <h3>Download the App</h3>
+            <p>
+              Discover food wherever and whenever and get your food delivered
+              quickly.
+            </p>
+            <div>
+              <Link></Link>
+              <Link></Link>
+            </div>
+          </div>
+          <img src="" alt="" />
+        </div>
+        <div className="footer_content">
+          <div className="logo">
+            <h3>Sushi</h3>
+            <img
+              src="https://png.pngtree.com/png-clipart/20220430/original/pngtree-sushi-logo-shop-png-image_7596643.png"
+              alt="sushi_samurai_logo"
+            />
+            <h3>Samurai</h3>
+          </div>
+          <ul>
+            <li>
+              <Link>About</Link>
+            </li>
+            <li>
+              <Link>Order</Link>
+            </li>
+            <li>
+              <Link>Menu</Link>
+            </li>
+            <li>
+              <Link>Membership</Link>
+            </li>
+            <li>
+              <Link>Download</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link>
+                <FaFacebook />
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <FaInstagram />
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <FaTwitter />
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <FaYoutube />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 };
