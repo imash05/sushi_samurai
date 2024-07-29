c11201727533410c7e5bb0a441726b2d9c5c21cd
IFer <76787367+imash05@users.noreply.github.com>
2024-07-29 21:10:22 +0200

    add code

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -73,12 +73,17 @@ const Home = () => {
       <AboutSushiSamurai />
       <div>food slider</div>
 
-
-
-      <div   className="sushi_ex"   >
+      <div className="sushi_ex">
         <div className="imgs">
-          <img src="" alt="" />
-          <img className="floatingImg" src="" alt="" />
+          <img
+            src="https://m.media-amazon.com/images/I/71UBT9tgdGL._AC_UF1000,1000_QL80_.jpg"
+            alt=""
+          />
+          <img
+            className="floatingImg"
+            src="https://m.media-amazon.com/images/I/71Tq21fHSgL._AC_UF894,1000_QL80_.jpg"
+            alt=""
+          />
         </div>
         <div className="xp_details">
           <h2>The Kura Experience</h2>
@@ -95,20 +100,19 @@ const Home = () => {
             </li>
             <li>
               <span>02</span>
-              <h3>Premium Ingredients</h3>
+              <h3>Authentic Japanese</h3>
               <p>
-                from sushi to broths and even deserts, all of our dishes are
-                free of artificial sweeteners,seasonings,preservatives and
-                colorings.
+                Our dishes adhere to Japanese virtue of serving natural and
+                healthy meals. We use only fresh premium ingredients for a
+                taste.
               </p>
             </li>
             <li>
               <span>03</span>
-              <h3>Premium Ingredients</h3>
+              <h3>Always Fresh & Bikkura-Pon</h3>
               <p>
-                from sushi to broths and even deserts, all of our dishes are
-                free of artificial sweeteners,seasonings,preservatives and
-                colorings.
+                Our potented Mr. Fresh ventilated suhsi lid and plate-tracking
+                tecnology ensures your food is always fresh and delicious.
               </p>
             </li>
           </ul>
